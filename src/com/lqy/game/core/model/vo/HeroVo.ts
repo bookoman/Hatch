d68200ed15e50330b0a857db9e5ef8f093669873
bookoman <bookoman@163.com>
2018-06-07 17:57:31 +0800

    英雄
    Signed-off-by: bookoman <bookoman@163.com>

---
+++ src/com/lqy/game/core/model/vo/HeroVo.ts
@@ -0,0 +1,50 @@
+/*
+* 宠物数据
+*/
+class HeroVo{
+    public heroId:string;
+    public level:number;
+    public exp:number;
+    public createTime:string;
+    public heroKey:string;
+    public levelKey:string;
+    public qualityKey:string;
+    public heroAttr:HeroAttr;
+    public heroTypeKey:string;
+    public atk:number;
+    public def:number;
+    public speed:number;
+    public hp:number;
+    /**性别 女 true,男 false */
+    public female:boolean;
+    /**是否交配 */
+    public coitus:boolean;
+    public maxExp:number;
+    constructor(){
+
+    }
+}
+
+/**生成属性 */
+class HeroAttr{
+    public hp:number;
+    public atk:number;
+    public def:number;
+    public speed:number;
+    public hitEffectRate:number;
+    public maxAtk:number;
+    public maxDef:number;
+    public maxHp:number;
+    public maxSpeed:number;
+    public upAtk:number;
+    public upDef:number;
+    public upHp:number;
+    public upSpeed:number;
+    public hitRate:number;
+    public evadeRate:number;
+    public resistRate:number;
+    public doubleAtkRate:number;
+    public hurtRate:number;
+    public tenacityRate:number;
+    public injuryRate:number;
+}
